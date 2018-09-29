2f100195b2984b809f520fcb82b6a0b5de99bad3
David Smith <dmsmith@Davids-MacBook-Pro.local>
2018-09-28 20:37:02 -0400

    Initialized Project

---
+++ MarvelChallenge/comicdetail/src/main/kotlin/com/marvel/dmsmith/marvelchallenge/comicdetail/ComicDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.marvel.dmsmith.marvelchallenge.comicdetail
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ComicDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_comic_detail)
+    }
+}
